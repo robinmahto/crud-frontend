42e745d26de70743e91ada1c7c04111dfab9a183
robinmahto <robinmahto413@gmail.com>
2022-11-24 13:39:15 +0530

    worked on form and user tables

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Form = () => {
+   const [state, setState] = useState({
+    username:"",
+    email:""
+   })
+
+   const handleChange = (e)=>{
+      e.preventDefault();
+      setState( (prevValue)=>( {...prevValue ,[e.target.name] : e.target.value}))
+   }
+
+   const onHandleClick = ()=>{
+      setState({
+        username:"",
+        email:""
+       })
+      console.log("state: ", state)
+   }
+
   return (
     <div className=' w-3/6 p-5 my-8 flex items-center justify-around'>
       <div className='border-0'>
-        <input type="text" placeholder='Enter your name' className='outline-0	px-4 py-2 bg-transparent border border-2 border-white-500 focus:outline-none text-white  placeholder:text-white rounded'  />
+        <input 
+           type="text" 
+           name='username'
+           value={state.username}
+           placeholder='Enter username' 
+           className='outline-0	px-4 py-2 bg-transparent border border-2 border-white-500 focus:outline-none text-white  placeholder:text-white rounded'  
+           onChange={handleChange}
+        />
       </div>
       <div>
-      <input type="email" placeholder='Enter your email' className='outline-0	px-4 py-2 bg-transparent border border-2 border-white-500 focus:outline-none text-white  placeholder:text-white rounded'  />
+      <input 
+         type="email" 
+         value={state.email}
+         name="email"
+         placeholder='Enter your email' 
+         className='outline-0	px-4 py-2 bg-transparent border border-2 border-white-500 focus:outline-none text-white  placeholder:text-white rounded' 
+         onChange={handleChange}
+      />
       </div>
       <div>
-        <button className='bg-white px-3 py-2 text-base	rounded	uppercase font-bold '>submit</button>
+        <button 
+           onClick={onHandleClick}
+           className='bg-white px-3 py-2 text-base	rounded	uppercase font-bold hover:bg-cyan-600 hover:text-white hover:duration-300	'>submit</button>
       </div>
-
     </div>
   )
 }
--- src/components/UserLists.js
+++ src/components/UserLists.js
@@ -3,22 +3,22 @@ import { FaEdit, FaTrashAlt } from 'react-icons/fa';
 
 const UserLists = () => {
   return (
-    <div className='bg-blue-300'>
-      <table>
-        <thead>
+    <div className='w-3/6'>
+      <table className='w-full border border-cyan-200'>
+        <thead className='text-white'>
           <tr>
-            <th>Name</th>
+            <th className='py-2'>Name</th>
             <th>Email</th>
             <th>Edit</th>
             <th>Delete</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className='text-center cursor-pointer text-white'>
           <tr>
-            <td>Robin Mahto</td>
+            <td className='py-3'>Robin Mahto</td>
             <td>robinmahto@gmail.com</td>
-            <td><FaEdit  /></td>
-            <td><FaTrashAlt /> </td>              
+            <td><button><FaEdit className='text-xl text-green-400	'/></button></td>
+            <td><button><FaTrashAlt className='text-xl text-red-500	'/></button> </td>              
           </tr>     
         </tbody>
       </table>
